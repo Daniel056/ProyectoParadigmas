8d583061ba045eb077c5b649eb4b51c6d44445fe
Daniel056 <zagadaniel06@gmail.com>
2018-10-22 16:57:38 -0600

    p

--- Proyecto2_Paradigmas/Proyecto2_Paradigmas.rkt
+++ Proyecto2_Paradigmas/Proyecto2_Paradigmas.rkt
@@ -1,1 +1,17 @@
 #lang racket
+;-----Proyecto #2 Paradigmas------------------
+;-----Funciones con Polinomios----------------
+;-----Intengrantes: Esteban Montero Fonseca---
+;------------------ Jefferson Moreno Zuñiga---
+;------------------ Daniel Zamora García------
+
+;Contruir el polinomio
+(define display-p
+  (lambda (L)
+    (cond ((null? L)L)
+          ((equal? 0 (car L)) display-p (cdr L))
+          ((equal? 0 (car(cdr L)) display-p ((cons (car L) (cdr(cdr L))))))
+          (else
+           (quote 1)))))
+          
+  
