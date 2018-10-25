3e65fc7e3da6050582b58fe1a6b7c48aeef5a4c7
Jeffez07 <32211215+Jeffez07@users.noreply.github.com>
2018-10-24 21:05:38 -0600

    Nuevo display-p

--- Proyecto2_Paradigmas/Proyecto2_Paradigmas.rkt
+++ Proyecto2_Paradigmas/Proyecto2_Paradigmas.rkt
@@ -5,34 +5,33 @@
 ;------------------ Jefferson Moreno Zuñiga---
 ;------------------ Daniel Zamora García------
 
+;--------------------------------------------------------------------------
 ;Contruir el polinomio
-; polynomial functions
-; NOTE: polynomials are represented inversely
-; i.e. '(1 2 3) => 1 + 2x + 3x^2
-
-; defunct
 (define display-p
-  (lambda (poly)
-    (letrec [(convert-to-strings
-              (lambda (ls i)
-                (if (null? ls)
-                    '()
-                    (cons
-                     (cond [(= i 0) (if (> (car ls) 0)
-                                        (string-append "+" (number->string (car ls)))
-                                        (string-append "-" (number->string (- (car ls)))))]
-                           [(= (car ls) 1) (string-append "+x^" (number->string i))]
-                           [#t (string-append 
-                                (if (> (car ls) 0)
-                                    (string-append "+" (number->string (car ls)))
-                                    (string-append "-" (number->string (- (car ls)))))
-                                (if (= i 1)
-                                    "x"
-                                    (string-append "x^" (number->string i))))])
-                     (convert-to-strings (cdr ls) (+ i 1))))))]
-      (foldl string-append "" (convert-to-strings poly 0)))))
+  (lambda (polinomio)
+    (cond
+      ((null? polinomio) "")
+      (else (cons-pol polinomio 0 "")))))
 
-
+(define cons-pol
+  (lambda (polinomio k resultado) 
+    (cond
+      ((null? polinomio) resultado)
+      ((= (car polinomio) 0) (cons-pol (cdr polinomio) (+ k 1) resultado))
+      ((> (car polinomio) 0)
+       (cond
+         ((= k 0) (cons-pol (cdr polinomio) (+ k 1) (string-append resultado (number->string (car polinomio)))))
+         ((= k 1) (cons-pol (cdr polinomio) (+ k 1) (string-append resultado (string-append "+" (string-append (number->string (car polinomio)) "x")))))
+         (else
+          (cons-pol (cdr polinomio) (+ k 1)
+                     (string-append resultado (string-append "+" (string-append (number->string (car polinomio)) (string-append "x^" (number->string k)))))))))
+       (else
+        (cond
+          ((= k 0) (cons-pol (cdr polinomio) (+ k 1) (string-append resultado (number->string (car polinomio)))))
+          ((= k 1) (cons-pol (cdr polinomio) (+ k 1) (string-append resultado (string-append (number->string (car polinomio)) "x"))))
+          (else
+           (cons-pol (cdr polinomio) (+ k 1)
+                      (string-append resultado (string-append (number->string (car polinomio)) (string-append "x^" (number->string k)))))))))))
 
 ;--------------------------------------------------------------------------
 ;Suma de Polinomios
