bb3aaa14793f99677492548705651cf2b8703df8
Jeffez07 <32211215+Jeffez07@users.noreply.github.com>
2018-10-28 15:47:43 -0600

    Factorizar
    funciona en algunos casos

--- Proyecto2_Paradigmas/Proyecto2_Paradigmas.rkt
+++ Proyecto2_Paradigmas/Proyecto2_Paradigmas.rkt
@@ -11,7 +11,20 @@
   (lambda (polinomio)
     (cond
       ((null? polinomio) "")
-      (else (cons-pol polinomio 0 "")))))
+      (else (cons-pol (simplifica polinomio) 0 "")))))
+
+(define simplifica
+  (lambda (polinomio)
+    (cond
+      ((null? polinomio) '())
+      (else (simplifica1 (reverse polinomio))))))
+
+(define simplifica1
+  (lambda (polinomio)
+    (cond
+      ((null? polinomio) polinomio)
+      ((= (car polinomio ) 0) (simplifica1 (cdr polinomio)))
+      (else (reverse polinomio)))))
 
 (define add
   (lambda (a b c d) 
@@ -58,14 +71,14 @@
   (lambda (polinomios)
     (cond
       ((null? polinomios) '())
-      ((null? (cddr polinomios)) (display-p (suma (car polinomios) (cadr polinomios))))
-      (else (+p (append (cddr polinomios) (cons (suma (car polinomios) (cadr polinomios)) '())))))))
+      ((null? (cddr polinomios)) (display-p (suma (simplifica (car polinomios)) (simplifica (cadr polinomios)))))
+      (else (+p (append (cddr polinomios) (cons (suma (simplifica (car polinomios)) (simplifica (cadr polinomios))) '())))))))
 
 (define suma
   (lambda (p1 p2)
     (cond
-      ((null? p1) p2)
-      ((null? p2) p1)
+      ((null? (simplifica p1)) (simplifica p2))
+      ((null? (simplifica p2)) (simplifica p1))
       (else
        (cons (+ (car p1) (car p2))
              (suma (cdr p1) (cdr p2)))))))
@@ -76,14 +89,14 @@
   (lambda (polinomios)
     (cond
       ((null? polinomios) '())
-      ((null? (cddr polinomios)) (display-p (resta (car polinomios) (cadr polinomios))))
-      (else (-p (append (cddr polinomios) (cons (resta (car polinomios) (cadr polinomios)) '())))))))
+      ((null? (cddr polinomios)) (display-p (resta (simplifica (car polinomios)) (simplifica (cadr polinomios)))))
+      (else (-p (append (cddr polinomios) (cons (resta (simplifica (car polinomios)) (simplifica (cadr polinomios))) '())))))))
 
 (define resta
   (lambda (p1 p2)
     (cond
-      ((null? p1) p2)
-      ((null? p2) p1)
+      ((null? (simplifica p1)) (simplifica p2))
+      ((null? (simplifica p2)) (simplifica p1))
       (else
        (cons (- (car p1) (car p2))
              (resta (cdr p1) (cdr p2)))))))
@@ -94,8 +107,8 @@
   (lambda (polinomios)
     (cond
       ((null? polinomios) '())
-      ((null? (cddr polinomios)) (display-p (multiplicacion (car polinomios) (cadr polinomios))))
-      (else (*p (append (cddr polinomios) (cons (multiplicacion (car polinomios) (cadr polinomios)) '())))))))
+      ((null? (cddr polinomios)) (display-p (multiplicacion (simplifica (car polinomios)) (simplifica (cadr polinomios)))))
+      (else (*p (append (cddr polinomios) (cons (multiplicacion (simplifica (car polinomios)) (simplifica (cadr polinomios))) '())))))))
 
 (define multiplicacion
   (lambda (pol1 pol2)
@@ -115,33 +128,45 @@
 ;División de Polinomios (Parte 1)
 (define qt-p
   (lambda (p1 p2)
+    (qt1 (simplifica p1) (simplifica p2) (simplifica p1) (simplifica p2) '())))
+
+(define qt1
+  (lambda (p1 p2 poli1 poli2 qt)
     (cond
-      ((null? p1) p2)
-      ((null? p2) p1)
-      ((= (car p2) 0) '(Division entre 0))
+      ((null? p1) (cond ((null? p2) qt) (else (append (cons poli1 '()) (cons poli2 '())))))
+      ((null? p2) (cond ((null? p1) qt) (else (append (cons poli1 '()) (cons poli2 '())))))
+      ((= (car p2) 0) (cond ((= (car p1) 0) (qt1 (cdr p1) (cdr p2))) (else (append (cons poli1 '()) (cons poli2 '())))))
+      ((= (car p1) 0) (cond ((= (car p2) 0) (qt1 (cdr p1) (cdr p2))) (else (append (cons poli1 '()) (cons poli2 '())))))
       (else
-       (cons (quotient (car p1) (car p2))
-             (qt-p (cdr p1) (cdr p2)))))))
+       (qt1 (cdr p1) (cdr p2) poli1 poli2 (append qt (cons (quotient (car p1) (car p2)) '())))))))
 
 ;--------------------------------------------------------------------------
 ;División de Polinomios (Parte 2)
 (define rem-p
   (lambda (p1 p2)
+    (rem1 (simplifica p1) (simplifica p2) (simplifica p1) (simplifica p2) '())))
+
+(define rem1
+  (lambda (p1 p2 pol1 pol2 rm)
     (cond
-      ((null? p1) p2)
-      ((null? p2) p1)
-      ((= (car p2) 0) '(Division entre 0))
+      ((null? p1) (cond ((null? p2) rm) (else (append (cons pol1 '()) (cons pol2 '())))))
+      ((null? p2) (cond ((null? p1) rm) (else (append (cons pol1 '()) (cons pol2 '())))))
+      ((= (car p2) 0) (cond ((= (car p1) 0) (rem1 (cdr p1) (cdr p2))) (else (append (cons pol1 '()) (cons pol2 '())))))
+      ((= (car p1) 0) (cond ((= (car p2) 0) (rem1 (cdr p1) (cdr p2))) (else (append (cons pol1 '()) (cons pol2 '())))))
       (else
-       (cons (remainder (car p1) (car p2))
-             (rem-p (cdr p1) (cdr p2)))))))
+       (rem1 (cdr p1) (cdr p2) pol1 pol2 (append rm (cons (remainder (car p1) (car p2)) '())))))))
 
 
 ;--------------------------------------------------------------------------
 ;División de Polinomios (Parte 3)
 (define /-p
   (lambda (p1 p2)
-    (append (cons (qt-p p1 p2) '())
-            (cons (rem-p p1 p2) '()))))
+    (div1 (simplifica p1) (simplifica p2))))
+
+(define div1
+  (lambda (p1 p2)
+    (cond ((= (length (qt-p p1 p2)) 2) (qt-p p1 p2))
+          (else (append (cons (qt-p p1 p2) '()) (cons (rem-p p1 p2) '()))))))
 
 ;--------------------------------------------------------------------------
 ;Derivación de Polinomios
@@ -156,12 +181,12 @@
   (lambda (polinomios derivadas)
     (cond
       ((null? polinomios) (reverse derivadas))
-      (else (drv-p1 (cdr polinomios) (append (cons (deriva (cdar polinomios) '() 1) '()) derivadas))))))
+      (else (drv-p1 (cdr polinomios) (append (cons (deriva (simplifica (cdar polinomios)) '() 1) '()) derivadas))))))
 
 (define deriva
   (lambda (polinomio derivada k)
     (cond
-      ((null? polinomio) derivada)
+      ((null? polinomio) (simplifica derivada))
       (else
        (deriva (cdr polinomio) (append derivada (cons (* (car polinomio) k) '())) (+ k 1))))))
 
@@ -169,13 +194,121 @@
 ;--------------------------------------------------------------------------
 ;Evaluación de Polinomios
 (define eval-p
+  (lambda (polinomio x)
+    (eval1 (simplifica polinomio) x)))
+
+
+(define eval1
   (lambda (poly x)
     (cond 
       ((= (length poly) 0) 0)
       ((= (length poly) 1) (car poly))
       (else 
-        (+ (car poly) (* x (eval-p (cdr poly) x)))))))
+        (+ (car poly) (* x (eval1 (cdr poly) x)))))))
 
 
 ;--------------------------------------------------------------------------
 ;Factorización de Polinomios
+(define divisores
+  (lambda (num div k)
+    (cond
+      ((= num 0) div)
+      ((> num 0)
+       (cond
+         ((= k 0) div)
+         (else
+          (cond ((= (remainder num k) 0) (divisores num (append div (cons k (cons (* -1 k) '()))) (- k 1)))
+                (else (divisores num div (- k 1)))))))
+      (else (divisores (* -1 num) div (* k -1))))))
+
+(define divisores1
+ (lambda (polinomio)
+   (d (simplifica polinomio))))
+
+(define d
+  (lambda (polinomio)
+   (cond
+     ((null? polinomio) 0)
+     (else
+      (cond
+        ((= (car polinomio) 0) 0)
+        (else
+         (divisores (car polinomio) '() (car polinomio))))))))
+;-------------------------------------------------------------------------
+(define raices
+  (lambda (pol divisores)
+    (cond
+      ((null? divisores) 0)
+      ((= (eval-p pol (car divisores)) 0) (append (cons 1 '()) (cons (* -1 (car divisores)) '())))
+      (else
+       (raices pol (cdr divisores))))))
+
+(define raices1
+  (lambda (polinomio)
+    (r (simplifica polinomio))))
+
+(define r
+  (lambda (polinomio)
+   (cond
+    ((null? polinomio) 0)
+    (else
+     (raices polinomio (divisores1 polinomio))))))
+;--------------------------------------------------------------------------
+(define ruffini
+  (lambda (polinomio factor raiz)
+    (cond
+      ((null? polinomio) (list (reverse factor)))
+      (else
+       (cond
+         ((= (+ (* raiz (car factor)) (car polinomio)) 0) factor)
+         (else (ruffini (cdr polinomio) (append (cons (+ (* raiz (car factor)) (car polinomio)) '()) factor) raiz)))))))
+
+(define ruffini1
+  (lambda (polinomio)
+    (ruff (simplifica polinomio))))
+
+(define ruff
+  (lambda (polinomio)
+    (cond
+      ((null? polinomio) 0)
+      (else
+       (ruffini (cdr (reverse polinomio)) (cons (car (reverse polinomio)) '()) (* -1 (car (reverse (raices1 polinomio)))))))))
+;--------------------------------------------------------------------------
+(define factor
+  (lambda (polinomio factores)
+    (cond
+      ((null? polinomio) factores)
+      (else
+       (append factores (cons (reverse (raices polinomio (divisores (car polinomio) '() (car polinomio))))
+         (cons (ruffini (cdr (reverse polinomio))
+               (cons (car (reverse polinomio)) '()) (* -1 (car (reverse (raices polinomio (divisores (car polinomio) '() (car polinomio))))))) '())))))))
+
+(define factor1
+  (lambda (polinomio factores)
+    (cond
+      ((null? polinomio) factores)
+      ((= (length polinomio) 2) (factor1 '() (append (append factores '()) (cons polinomio '()))))
+      (else
+       (factor1 (ruffini1 polinomio) (append factores (cons (reverse (raices1 polinomio)) '())))))))
+
+(define fact-p
+  (lambda (polinomio)
+    (cond
+      ((null? polinomio) polinomio)
+      (else
+       (factor1 (simplifica polinomio) '())))))
+;--------------------------------------------------------------------------
+(define common-f
+  (lambda (polinomio cf)
+    (cond
+      ((null? polinomio) cf)
+      ((= (length polinomio) 1) (gcd (car polinomio) cf))
+      (else
+       (common-f (cdr polinomio) (gcd (cadr polinomio) cf))))))
+
+(define gcd
+  (lambda (a b)
+  (cond ((= b 0) a)
+      (else (gcd b (modulo a b))))))
+ 
+
